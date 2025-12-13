1332a93ce37d9422e39edc42594e61b0c842d887
bashensanbuqu <64031631+bashensanbuqu@users.noreply.github.com>
2025-12-14 00:57:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-author=v2
+author=bashensanbuqu
 # github=https://github.com/233boy/v2ray
 
 # bash fonts colors
@@ -70,7 +70,7 @@ is_conf_dir=$is_core_dir/conf
 is_log_dir=/var/log/$is_core
 is_sh_bin=/usr/local/bin/$is_core
 is_sh_dir=$is_core_dir/sh
-is_sh_repo=233boy/$is_core
+is_sh_repo=$author/$is_core
 is_pkg="wget unzip"
 is_config_json=$is_core_dir/config.json
 tmp_var_lists=(
